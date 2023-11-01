5dc85e474bde93f747ea0afe11a8527f96dfb1f5
OualidEssaidi <oualid_essaidi@hotmail.com>
2023-11-01 21:15:54 +0100

    Adding simulation part

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//Burger menu
+
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("open-menu");
 var closeBtn = document.getElementById("closeBtn");
@@ -5,12 +7,10 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
 function openNav() {
 sidenav.classList.add("active");
 }
-
-/* Set the width of the side navigation to 0 */
 function closeNav() {
 sidenav.classList.remove("active");
 }
+
