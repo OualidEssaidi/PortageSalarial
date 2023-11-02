28c57152d8bf20d9fe31ad22273c1c14afb5ed3a
OualidEssaidi <oualid_essaidi@hotmail.com>
2023-11-02 17:35:22 +0100

    Client feedback part finished

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 //Burger menu
 
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("open-menu");
-var closeBtn = document.getElementById("closeBtn");
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("closeBtn");
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
@@ -14,3 +14,44 @@ function closeNav() {
 sidenav.classList.remove("active");
 }
 
+
+
+const slider = document.querySelector(".items");		
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
+
+let current = 0;
+let prev = 2;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+	button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 2 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+	current = number;
+	prev = current - 1;
+	next = current + 1;
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].classList.remove("active");
+		slides[i].classList.remove("prev");
+		slides[i].classList.remove("next");
+	}
+
+	if (next == 3) {
+		next = 0;
+	}
+
+	if (prev == -1) {
+		prev = 2;
+	}
+
+	slides[current].classList.add("active");
+	slides[prev].classList.add("prev");
+	slides[next].classList.add("next");
+}
